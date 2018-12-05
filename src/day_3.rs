b489e7586d7fc6118e6f41ee8f0db4b5d4d2b8c0
Matt Bray <mattjbray@gmail.com>
2018-12-05 21:35:09 +0000

    day_3: parse claims

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,3 +1,10 @@
+pub fn run(path: &str) {
+    let input = std::fs::read_to_string(path).expect("Couldn't read data file.");
+
+    let (_, claims) = parse_claims(&input).expect("Couldn't parse claims");
+    println!("{:?}", claims)
+}
+
 #[derive(Debug, PartialEq)]
 struct Claim {
     id: u32,
@@ -12,11 +19,17 @@ struct Rectangle {
     height: u32,
 }
 
+#[derive(Debug, PartialEq)]
+struct Square {
+    x: u32,
+    y: u32,
+}
+
 named!(parse_u32 <&str, u32>,
-       map_res!(nom::digit, std::str::FromStr::from_str)
+    map_res!(nom::digit, std::str::FromStr::from_str)
 );
 
-named!(claim(&str) -> Claim,
+named!(parse_claim(&str) -> Claim,
     do_parse!(
         tag!("#") >>
         id: terminated!(parse_u32, tag!(" @ ")) >>
@@ -33,11 +46,15 @@ named!(claim(&str) -> Claim,
         } )
        ));
 
+named!(parse_claims(&str) -> Vec<Claim>,
+       terminated!(separated_list_complete!(char!('\n'), parse_claim), tag!("\n"))
+);
+
 #[test]
 fn test_claim_parser() {
     assert_eq!((parse_u32("123 ")), Ok((" ", 123)));
     assert_eq!(
-        claim("#123 @ 3,2: 5x4 "),
+        parse_claim("#123 @ 3,2: 5x4 "),
         Ok((
             " ",
             Claim {
@@ -51,4 +68,39 @@ fn test_claim_parser() {
             }
         ))
     );
+}
+
+#[test]
+fn test_claims_parser() {
+    let claims_str = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2", ""].join("\n");
+    let expected_claims = vec![
+        Claim {
+            id: 1,
+            rectangle: Rectangle {
+                left: 1,
+                top: 3,
+                width: 4,
+                height: 4,
+            },
+        },
+        Claim {
+            id: 2,
+            rectangle: Rectangle {
+                left: 3,
+                top: 1,
+                width: 4,
+                height: 4,
+            },
+        },
+        Claim {
+            id: 3,
+            rectangle: Rectangle {
+                left: 5,
+                top: 5,
+                width: 2,
+                height: 2,
+            },
+        },
+    ];
+    assert_eq!(parse_claims(&claims_str), Ok(("", expected_claims)));
 }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     match day.as_ref() {
         "1" => aoc::day_1::run(&args[2]),
         "2" => aoc::day_2::run(&args[2]),
+        "3" => aoc::day_3::run(&args[2]),
         _ => println!("I haven't solved that yet :("),
     }
 }
