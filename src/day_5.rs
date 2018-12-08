5fecc58c263eaddbe2df29afd959c9133e12f912
Matt Bray <mattjbray@gmail.com>
2018-12-08 22:49:53 +0000

    solve day 5 part 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,8 +1,13 @@
+use std::collections::HashMap;
+
 pub fn run(path: &str) {
     let input = std::fs::read_to_string(path).expect("Couldn't read data file.");
 
     let part_1_solution = react(&input);
     println!("day 5, part 1: {:?}", part_1_solution.len());
+
+    let part_2_solution = part_2(&input);
+    println!("day 5, part 2: {:?}", part_2_solution);
 }
 
 fn units_react(c1: char, c2: char) -> bool {
@@ -36,6 +41,25 @@ fn react(polymer: &str) -> String {
     }
 }
 
+fn part_2(polymer: &str) -> Option<(char, usize)> {
+    let mut map: HashMap<char, usize> = HashMap::new();
+
+    let chars = "abcdefghijklmnopqrstuvwxyz";
+
+    for c in chars.chars() {
+        let input = polymer.replace(c, "").replace(c.to_ascii_uppercase(), "");
+        let result = react(&input);
+        map.insert(c, result.len());
+    }
+
+    let ans = map
+        .iter()
+        .min_by(|(_, v1), (_, v2)| v1.cmp(v2))
+        .map(|(&c, &len)| (c, len));
+
+    ans
+}
+
 #[cfg(test)]
 
 mod test {
@@ -48,5 +72,10 @@ mod test {
         assert_eq!(react("abAB"), "abAB".to_string());
         assert_eq!(react("aabAAB"), "aabAAB".to_string());
         assert_eq!(react("dabAcCaCBAcCcaDA"), "dabCBAcaDA".to_string());
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part_2("dabAcCaCBAcCcaDA"), Some(('c', 4)));
     }
 }
